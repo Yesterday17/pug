a15e39cf744be217d86f985dd9cc78909f1e1d37
Yesterday17 <t@yesterday17.cn>
2019-12-13 23:28:22 +0800

    Basic Pipeline api.

---
+++ api/linear_pipeline.go
@@ -0,0 +1,57 @@
+/**
+MIT License
+
+Copyright (c) 2019 Yesterday17
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package api
+
+import "container/list"
+
+type linearPipeline struct {
+	line *list.List
+}
+
+func NewLinearPipeline() Pipeline {
+	return &linearPipeline{
+		line: list.New(),
+	}
+}
+
+func (l *linearPipeline) Append(p Pipe) {
+	l.line.PushBack(p)
+}
+
+func (l *linearPipeline) Run() {
+	var prev Pipe = nil
+	for l.line.Len() > 0 {
+		current := l.line.Front()
+		l.line.Remove(current)
+
+		p := (current.Value).(Pipe)
+		p.Do(prev)
+		prev = p
+
+		if p.Type() == EndpointPipe {
+			break
+		}
+	}
+}
--- main/pug/main.go
+++ main/pug/main.go
@@ -1,1 +1,34 @@
-package pug
+/**
+PUG
+Copyright (C) 2019  Yesterday17
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package main
+
+import (
+	"github.com/Yesterday17/pug/api"
+	"github.com/Yesterday17/pug/modules"
+)
+
+func main() {
+	pl := api.NewLinearPipeline()
+	// TODO: Remove test
+	pl.Append(modules.Modules["shell"](map[string]interface{}{
+		"cmd":  "date",
+		"args": []string{},
+	}).(api.Pipe))
+	pl.Run()
+}
